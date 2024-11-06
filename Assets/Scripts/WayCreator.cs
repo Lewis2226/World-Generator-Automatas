5ef36af8105deb384cd17d176eaf8257a93b05b7
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-11-05 19:57:46 -0600

    base de AStar

---
+++ Assets/Scripts/WayCreator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayCreator 
+{
+    void FindWay(Transform StartPos, Transform EndPos)
+    {
+
+    }
+
+    void CreateWay(GameObject wayPrefab)
+    {
+
+    }
+}
