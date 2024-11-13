04d2f8b49b67ef0aa7fc2fceb6a89db01e3fc1da
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-11-13 10:58:45 -0600

    ajustes para poder hacer la build

--- Assets/Scripts/Level Map/LevelGenerator.cs
+++ Assets/Scripts/Level Map/LevelGenerator.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 public class LevelGenerator : MonoBehaviour
 {
